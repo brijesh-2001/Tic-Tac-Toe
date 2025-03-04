a4c16341e0fea0807733c28fc6cc3c808980e75f
brijesh-2001 <Brijeshrawat2001@gmail.com>
2025-03-04 11:49:07 +0530

    Change of port in viteconfig file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Ensures the app is accessible externally
+    port: process.env.PORT || 3000, // Uses the port specified by Render, or defaults to 3000
+  },
+});
